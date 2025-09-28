d3494fa882103e08fb954df17ce8301064b7a164
SriniPallapotu <pallapotu75@gmail.com>
2025-09-28 21:10:46 +1000

    Task3
    Maintain code and Added comments for each function

--- src_code/main.py
+++ src_code/main.py
@@ -1,6 +1,15 @@
 #import Geopands library 
 import geopandas as gpd
 
+""" 
+The Objective of this Python program is to 
+    1. Read Vector Spatial Data file (Eg:NSW state Public Hospitals Information) into GeoDataFrame
+    2. Reproject the Dataset from its Original Coordinate Reference System (CRS) to specified EPSG code
+       and export the resulting new Shapefile to output folder.
+    3. Print the Attribute Table of Original GeoDataFrame
+This script performs error handling with Try / Except and Basic GeoPandas operations
+"""
+
 #Set file path to read orginal spatial source data
 FILE_PATH = r"C:\Python_Programming_TAFE\ICTPRG440\spatial_data_original\NSW_MyHospitals_Public_SriniP\MyHospital_Sites.shp"
 
@@ -10,6 +19,17 @@ OUTPUT_PATH = r"C:\Python_Programming_TAFE\ICTPRG440\output"
 #1.	Define a function to read a vector spatial data and return a geodataframe object
 
 def readSpatialData(filepath):
+    """ 
+    This function reads spatial data from input shapefile and returns the GeoDataFrame
+
+    Parameters: 
+
+        Paramaeter:Datatype
+        filepath : str
+            input fullpath to read source spatial data file (Eg: .shp)
+
+    Returns GeoDataFrame: gpd.GeoDataFrame
+    """
     try:
         if isinstance(filepath,str):
             pass
@@ -24,17 +44,28 @@ gdf=readSpatialData(FILE_PATH)
 
 #print(gdf.head())
 
-def PrintAttributeTable(gdf):
+#2. Print Attribute Table of GeoDataFrame
+
+def PrintAttributeTable(input_gdf):
+    """
+    This funtion prints attribute table (all rows and all Coloumns) of input GeoDataFrame
+
+    Parameters:
+        input_gdf:gpd.GeoDataFrame
+
+    Returs None
+    
+    """
     try:
 
-        if isinstance(gdf,gpd.geodataframe.GeoDataFrame):
+        if isinstance(input_gdf,gpd.geodataframe.GeoDataFrame):
             pass
         else:
             raise ValueError("Input is not a GeoDataFrame")
-        for row in gdf.itertuples():
+        for row in input_gdf.itertuples():
             print(row)
     except Exception as e:
-        print(f"[PrintAttributeTable] Errot:",{e})   
+        print(f"[PrintAttributeTable] Error:",{e})   
 
 
     return None
@@ -47,7 +78,22 @@ def PrintAttributeTable(gdf):
 #Check Coordinate Reference System for Input spatial Source data
 #print(gdf.crs)
 
+#3. Transform (Reproject) original CRS into Targeted CRS
+
 def trans_From_Web_MTo_Geog_WGS84(inputfilepath,outputpath,EPSG=4326):
+    """
+    This funtion reprojects the origianl spatial dataset CRS to target CRS and export it to new shapefile.
+
+    parameters:
+        inputfilepath:str
+            fullpath to input spatialfile to be reprojected
+        outputpath:str
+            fullpath to directory where reprojected file will be saved
+        EPSG:int
+             EPSG code of target CRS
+    Returns None
+
+    """
     try:
         if isinstance(EPSG,int):
             pass
@@ -72,4 +118,7 @@ def trans_From_Web_MTo_Geog_WGS84(inputfilepath,outputpath,EPSG=4326):
     except Exception as e:
         print(f"[trans_From_Web_MTo_Geog_WGS84] Errot:",{e})
     return None
-trans_From_Web_MTo_Geog_WGS84(inputfilepath=FILE_PATH,outputpath=OUTPUT_PATH,EPSG=4326)
+#trans_From_Web_MTo_Geog_WGS84(inputfilepath=FILE_PATH,outputpath=OUTPUT_PATH,EPSG=4326)
+
+if __name__=="__main__":
+    trans_From_Web_MTo_Geog_WGS84(inputfilepath=FILE_PATH,outputpath=OUTPUT_PATH,EPSG=4326)
